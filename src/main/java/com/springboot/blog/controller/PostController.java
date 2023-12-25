befd83be349f334393b403597b2e1b99df151a84
Shubham Pandey <shwillshine@gmail.com>
2023-12-25 16:16:07 +0530

    Created API for Read Update and Delete and checked its working on PostMan

--- src/main/java/com/springboot/blog/controller/PostController.java
+++ src/main/java/com/springboot/blog/controller/PostController.java
@@ -4,10 +4,9 @@ import com.springboot.blog.payload.PostDto;
 import com.springboot.blog.service.PostService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/posts")
@@ -26,4 +25,39 @@ public class PostController {
     {
         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
     }
+
+    //get all post REST API
+
+    @GetMapping("/allPosts")
+    public List<PostDto> getAllPosts(){
+         return postService.getAllPost();
+    }
+
+    //get post by id REST API
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PostDto> getPostById(@PathVariable(name="id") long id)
+    {
+        return ResponseEntity.ok(postService.getPostById(id));
+    }
+
+    //Update post by id
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PostDto> updatePost(@RequestBody PostDto postDto,@PathVariable(name="id") long id){
+
+     PostDto postResponse= postService.updatePost(postDto,id);
+
+        return new ResponseEntity<>(postResponse,HttpStatus.OK);
+    }
+
+    //delete post REST API
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletePost(@PathVariable(name="id") long id){
+          postService.deletePostById(id);
+          return new ResponseEntity<>("Post Deleted Succesfully",HttpStatus.OK);
+    }
+
+
 }
